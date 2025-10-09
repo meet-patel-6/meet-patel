e6f0ef5fa3c794e5f57061d0f48c8e192951a7ed
Meet Thummar <thummarmeet15@gmail.com>
2025-10-10 00:42:06 +0200

    added basename in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { basename } from "path";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/meet-patel/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
